b07bed55cebb5603e04d5864aeced8fe0491fff2
kimobot <34517225+kimobot@users.noreply.github.com>
2020-08-01 20:20:01 +0200

    Add Nova and publish files. Remove default Laravel welcome view and add empty home view.

---
+++ resources/views/home.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Home</title>
+
+        <!-- Fonts -->
+
+        <!-- Styles -->
+    </head>
+    <body>
+        <div class="flex-center position-ref full-height">
+          
+        </div>
+    </body>
+</html>
