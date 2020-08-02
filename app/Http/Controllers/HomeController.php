965ced39b64ac2b3afdbdce50f013fc86e7186af
kimobot <34517225+kimobot@users.noreply.github.com>
2020-08-02 10:07:01 +0200

    Set correct timezone. Display artworks whose publish_on time has passed.

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,13 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Artwork;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
-use App \Artwork;
 
 class HomeController extends Controller
 {
     public function home() {
-        $artworks = Artwork::all();
+        $artworks = Artwork::where('publish_on', '<' , Carbon::now())->get();
         return view('home')->with(['artworks' => $artworks]);
     }
 }
