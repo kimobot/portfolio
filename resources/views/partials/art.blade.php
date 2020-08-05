f8f7d9db0aea72a029af2be2b5e22f47628b563d
kimobot <34517225+kimobot@users.noreply.github.com>
2020-08-05 08:42:49 +0200

    Remove art title/description from gallery cards.

--- resources/views/partials/art.blade.php
+++ resources/views/partials/art.blade.php
@@ -4,12 +4,6 @@
         @foreach($artworks as $artwork)
         <div @click.prevent="open = true, modalSrc='{{ asset('storage/' . $artwork->image) }}', modalTitle='{{ $artwork->title }}', modalDesc='{{ $artwork->description }}'" class="rounded overflow-hidden shadow-lg">
             <img class="w-full" src="{{ asset('storage/' . $artwork->image) }}" alt="">
-            <div class="px-6 py-4">
-                <div class="font-bold text-xl mb-2">{{ $artwork->title }}</div>
-                <p class="text-gray-700 text-base">
-                    {{ $artwork->description }}
-                </p>
-            </div>
             {{-- <div class="px-6 py-4">
                 <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#photography</span>
                 <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#travel</span>
