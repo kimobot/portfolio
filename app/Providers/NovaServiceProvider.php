7b121c4bc9b911fe0676aa5013d97c5c282f2082
kimobot <34517225+kimobot@users.noreply.github.com>
2020-08-14 20:27:10 +0200

    Move email to .env.

--- app/Providers/NovaServiceProvider.php
+++ app/Providers/NovaServiceProvider.php
@@ -43,7 +43,7 @@ class NovaServiceProvider extends NovaApplicationServiceProvider
     {
         Gate::define('viewNova', function ($user) {
             return in_array($user->email, [
-                'kim.botticelli@gmail.com'
+                env('NOVA_EMAIL')
             ]);
         });
     }
